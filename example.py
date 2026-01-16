b04d55391d224305c5fb2467c8df0ae30a6795c7
Bright-2010 <zhenghongyu102030@outlook.com>
2026-01-16 09:03:36 +0800

    Change API endpoint and enhance user feedback
    Updated API endpoint to use localhost and improved error messaging.

--- example.py
+++ example.py
@@ -3,18 +3,24 @@ import tkinter as tk
 from tkinter import ttk
 def re_num():
     http=requests.get(
-        "https://api.brightweb.top/trig",
+        #"https://api.brightweb.top/trig",
+        "http://localhost:3000/trig",
         params={
             "type": var.get(),
             "deg": enter_var.get()
         }
     )
-    print(http.json())
-    answer_lable.config(text="结果是:"+str(http.json()['result']))
+    var_answer=http.json()
+    print(var_answer)
+    answer_lable.config(text="结果是:"+str(var_answer['result']))
+
+    
 
 win=tk.Tk()
 win.title("三角函数计算器")
 win.geometry("400x500")
+win.attributes("-topmost", True)
+#win.after(200, lambda: win.attributes("-topmost", False))
 tk.Label(text="三角函数计算器",font=(None,18)).pack()
 
 var=tk.StringVar()
@@ -31,4 +37,6 @@ OK_button.pack()
 
 answer_lable=tk.Label(text="结果是:",font=(None,16))
 answer_lable.pack()
+tk.Label(text="如果点击 计算 不出结果，可能是API出现问题以及你的网络问题").pack()
+tk.Label(text="如果出现问题可以在控制台查看日志").pack()
 win.mainloop()
